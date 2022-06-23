054084c860e2d6e91b6a2c078f561e6e280513dc
Tishchenko_D <tishchenko.1993@list.ru>
2022-06-23 14:40:44 +0300

    Example 005 If Else

---
+++ Example005_ifelse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string username = Console.ReadLine();
+if(username.ToLower()=="катя")
+{
+    Console.WriteLine("Ура, этоже Катя!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
